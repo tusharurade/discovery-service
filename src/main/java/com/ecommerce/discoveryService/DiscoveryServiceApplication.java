08d2b1604f224983742746e1b4541bfd8c118df7
Tushar <tushar.urade@gmail.com>
2026-05-12 23:42:50 +0530

    rebuild services configuration

--- src/main/java/com/ecommerce/discoveryService/DiscoveryServiceApplication.java
+++ src/main/java/com/ecommerce/discoveryService/DiscoveryServiceApplication.java
@@ -1,4 +1,4 @@
-package com.ecommerce.discoveryService;
+package com.ecommerce.discoveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
